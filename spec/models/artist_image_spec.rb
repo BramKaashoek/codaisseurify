d756a8d0e4f83c412485ca519eb98bf6079752f9
bram <bramkaashoek@gmail.com>
2017-05-25 12:50:00 +0200

    added devise and wrote tests for models

--- spec/models/artist_image_spec.rb
+++ spec/models/artist_image_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ArtistImage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:image) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:artist) }
+  end
 end
