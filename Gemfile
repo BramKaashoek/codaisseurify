79bdf0b37e70c8b5795c8ed55d6dfb9b325c552b
bram <bramkaashoek@gmail.com>
2017-05-25 11:23:21 +0200

    set up initial feature list

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '2.4.1'
 
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
@@ -61,3 +62,4 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'jquery-rails', '~> 4.3.1'
 gem 'carrierwave', '0.11.2'
 gem 'cloudinary', '1.2.3'
+gem 'rails_12factor', group: :production
---
+++ spec/features/show_artists.html.erb_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+describe "artist show page" do
+  it "shows information on the band"
+  it "show all the bands songs"
+  it "visitors can add songs to the artist"
+  it "visitors can delete the artists songs"
+  it "visitors can delete the artist, including all their songs"
+  it "#optional: require user to register and log in to modify artist page"
+  it "#optional: show the artists genre, clickable to a genre description page"
+  it "#optional: add support for albums"
+end
