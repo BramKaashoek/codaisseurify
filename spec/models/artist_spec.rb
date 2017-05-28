f8aae546c3e6f72eda76213ef11c8363d7d7a88e
bram <bramkaashoek@gmail.com>
2017-05-28 15:12:08 +0200

    updated testing, added sorting2

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -12,33 +12,4 @@ RSpec.describe Artist, type: :model do
     it { should have_many(:songs) }
     it { should have_one (:artist_image) }
   end
-
-  let!(:abbath)    { create :artist, name: "Abbath" }
-  let!(:zztop)     { create :artist, name: "ZZ Top" }
-  let!(:katatonia) { create :artist, name: "Katatonia" }
-
-  describe ".order_by_name" do
-    it "returns array of bands ordered by name" do
-      expect(Artist.order_by_name).to match_array [abbath, katatonia, zztop]
-    end
-  end
-
-  describe ".reverse_order_by_name" do
-    it "returns array of bands reverse ordered by name" do
-      expect(Artist.order_by_name).to match_array [ zztop, katatonia, abbath]
-    end
-  end
-
-  describe ".order_by_creation_date" do
-    it "returns array of bands ordered by creation datetime" do
-      expect(Artist.order_by_creation_date).to match_array [abbath, zztop, katatonia]
-    end
-  end
-
-  describe ".reverse_order_by_creation_date" do
-    it "returns array of bands reverse ordered by creation datetime" do
-      expect(Artist.order_by_creation_date).to match_array [zztop, abbath,  katatonia]
-    end
-  end
-
 end
