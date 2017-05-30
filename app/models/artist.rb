e8bcbd4b90b6e66c9118997fc922648a2f6377d0
bram <bramkaashoek@gmail.com>
2017-05-30 22:13:56 +0200

    testing updated, everything should be working now

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,8 @@
 class Artist < ApplicationRecord
   mount_uploader :image, ImageUploader
-  
+
   has_many :albums, dependent: :destroy
   has_many :songs, through: :albums
 
   validates :name, :genre, :country, :image, presence: true
-
-
 end
