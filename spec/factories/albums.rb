eeeccff889bb197ffc3105f641d6e9616d083c23
bram <bramkaashoek@gmail.com>
2017-06-04 21:48:09 +0200

    initial feature testing

--- spec/factories/albums.rb
+++ spec/factories/albums.rb
@@ -1,9 +1,8 @@
 FactoryGirl.define do
   factory :album do
-
     name { Faker::Name.first_name }
-    image "http://img05.deviantart.net/5722/i/2013/035/4/9/vomiting_gnome_meme_by_soplon-d5tup49.png"
-
+    year { Faker::Number.between(1000, 2050)}
+    image { Faker::Internet.url('example.com', '/foobar.png') }
     artist { build :artist}
   end
 end
---
+++ spec/features/remove_song_ajax_sec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe "remove a song and have the artist view updated " do
+
+
+end
