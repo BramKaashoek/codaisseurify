367c49fe3695c2220e419b81877d53d355025caf
bram <bramkaashoek@gmail.com>
2017-06-05 01:11:05 +0200

    more minor shit

--- spec/features/add_song_ajax_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-describe "add a song and have it added to the artist page" do
-
-
-end
--- spec/features/remove_song_ajax_sec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-describe "remove a song and have the artist view updated " do
-
-
-end
